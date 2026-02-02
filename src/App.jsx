6ea0191d4b109bafefd4c515512348a8d8749e4f
quynhnhi <quynhngocnhi28@gmail.com>
2026-02-02 11:02:28 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import "./App.css";
 import SubjectList from "./SubjectList";
 
 function App() {
-  const Subjects = ["ReactJS", "NodeJS", "Java", "Database"];
+  const subjects = ["ReactJS", "NodeJS", "Java", "Database"];
 
   return (
       <div className='app-container'>
         <h1>Ứng dụng ReactJS</h1>
         <SubjectList subjects={subjects}/>
       </div>
-  )
+  );
 }
 
 export default App;
